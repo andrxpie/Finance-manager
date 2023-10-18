3f97e72062d0557526155398ab0bd1d6901ecaea
Макс Конончук <eexet17@gmail.com>
2023-10-18 20:47:34 +0300

    create authorization windows

---
+++ Authorization.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Finance_manager
+{
+    /// <summary>
+    /// Interaction logic for Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        public string login = string.Empty;
+        public string password = string.Empty;
+
+        public Authorization()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CreateAccBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAccountWindow createAccountWindow = new CreateAccountWindow();
+            Close();
+            createAccountWindow.Show();
+        }
+
+        private void LogInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //"MainWindow" mw = new "MainWindow"(login, password);
+            Validation();
+            
+            //mw.Show();
+        }
+        private void Validation()
+        {
+            if (loginTxtBox.Text != string.Empty && passTxtBox.Text != string.Empty)
+            {
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect login or password");
+                
+            }
+        }
+    }
+}
---
+++ CreateAccountWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Finance_manager
+{
+    /// <summary>
+    /// Interaction logic for CreateAccountWindow.xaml
+    /// </summary>
+    public partial class CreateAccountWindow : Window
+    {
+        public CreateAccountWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateAccBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CheckValid();
+        }
+        private void CheckValid()
+        {
+            
+            if (passTxtBox.Text == passSecTxtBox.Text)
+            {
+                //save in db
+                Authorization auth = new Authorization();
+
+                Close();
+
+                //in db emailTxtBox.Text;
+                auth.login = this.loginTxtBox.Text;
+                auth.password = this.passTxtBox.Text;
+                auth.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid login or password");
+            }
+        }
+    }
+}
