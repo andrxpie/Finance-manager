3a3de316edf1c1c9dcc00255a22e0a12de25f59e
Андрій Грицюк <venganpro12@gmail.com>
2023-10-20 18:08:43 +0300

    branch done

---
+++ FinanceManagerDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance_manager
+{
+    public class FinanceManagerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CategoryCfg());
+            modelBuilder.ApplyConfiguration(new TransactionCfg());
+            modelBuilder.ApplyConfiguration(new UserCfg());
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
